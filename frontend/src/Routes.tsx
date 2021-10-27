11add4220371ea00a97c3f22aa27173811956d8b
pedroaleph <pedro.aleph.allen@gmail.com>
2021-10-26 21:54:44 -0400

    cleaning frontend

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
