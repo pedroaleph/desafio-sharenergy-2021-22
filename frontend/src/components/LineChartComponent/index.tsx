b2c359d5fc2f96beef90442578ebf3da08674e1b
pedroaleph <pedro.aleph.allen@gmail.com>
2021-10-30 22:08:38 -0400

    adapting time

--- frontend/src/components/LineChartComponent/index.tsx
+++ frontend/src/components/LineChartComponent/index.tsx
@@ -9,7 +9,12 @@ import {
     Tooltip,
   } from "recharts";
 
+const timeFormatter = (time: number) => {
+    const hour = Math.trunc(time);
+    const min = Math.fround((Math.abs(time) - hour) * 60);
 
+    return 'Tempo: ' + hour + 'h e ' +  min + 'min';
+}
 
 const LineChartComponent = ({ content }: PlantData) => {
 
@@ -17,7 +22,6 @@ const LineChartComponent = ({ content }: PlantData) => {
     <LineChart
         width={700}
         height={300}
-        data={content}
         margin={{
         top: 5,
         right: 30,
@@ -26,13 +30,16 @@ const LineChartComponent = ({ content }: PlantData) => {
     }}
     >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis type="number" dataKey="tempo_h" name="tempo" unit="h" domain={[0, 24]} tickCount={9} />
-        <YAxis type="number" dataKey="tensao_V" name="tensão" unit="V" />
+        <XAxis type="number" dataKey="tempo_h" unit="h" domain={[0, 24]} tickCount={9} />
+        <YAxis type="number" dataKey="tensao_V" unit="V" />
         <Tooltip
+            formatter={(value: number , name :string) => [`${value} volts` ,"Tensão"]}
+            labelFormatter={(label) => timeFormatter(label)}
         />
         <Line
             type="monotone"
-            dataKey='tensao_V'
+            data={content}
+            dataKey={"tensao_V"}
             stroke="#42A3A3"
             dot={false}
         />
