bb5446ce1fbc37837252654393eb73465f26f677
pedroaleph <pedro.aleph.allen@gmail.com>
2021-11-03 14:02:14 -0400

    initializing backend

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server Running here 👉 https://localhost:${PORT}`);
+  });
+  
+
+
+
+
+
